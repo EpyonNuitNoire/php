15425addb65ca35505aca1d2ed7f02d0efafa437
EpyonNuitNoire <geroferrevazquez@gmail.com>
2024-06-03 19:41:28 +0200

    Ejercicios Actualizados

--- ejercicios.php
+++ ejercicios.php
@@ -1,12 +1,52 @@
 <?php
 $Calificación = 50;
- if ($Calificación >= 90){
+ 
+if ($Calificación >= 90){
  echo "Tu Calificación es A.";
- }else if ($Calificación >= 80){
+ 
+}elseif ($Calificación >= 80){
  echo "Tu Calificación es B.";
- }else if ($Calificación >= 70) {
+
+}elseif ($Calificación >= 70) {
   echo "Tu Calificación es C.";
- }else if ($Calificación < 70) {
+
+}elseif ($Calificación < 70) {
 echo "Tu Calificación es F.";      
-  }
+}
+
+?>
+
+<?php
+$Hora = 0;
+
+if ($Hora < 12){
+echo "Buenos dias.";
+
+}elseif ($Hora >= 12){
+echo "Buenas tardes.";
+
+}elseif ($Hora > 18 ) {
+  echo "Buenas noches.";
+       
+ }
+
+
+?>
+
+
+<?php
+$Edad = 0;
+
+if ($Edad < 18){
+echo " Menor de edad.";
+
+}elseif ($Edad >= 18){
+echo "Adulto.";
+
+}elseif ($Edad >= 65) {
+  echo "Persona mayor.";
+       
+ }
+
+
 ?>
